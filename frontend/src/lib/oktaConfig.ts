dade5e478f4d4e744b1487e84658f92327ecfe79
ezzyouy <82385007+ezzyouy@users.noreply.github.com>
2025-01-03 19:56:34 +0100

    Update oktaConfig.ts

--- frontend/src/lib/oktaConfig.ts
+++ frontend/src/lib/oktaConfig.ts
@@ -1,6 +1,6 @@
 export const oktaConfig = {
-  clientId: "0oam9m9p0dJw8YeBS5d7",
-  issuer: "https://dev-35138089.okta.com/oauth2/default",
+  clientId: "..........",
+  issuer: "https://dev-{....}.okta.com/oauth2/default",
   redirectUri: "http://localhost:3000/login/callback",
   scopes: ["openid", "profile", "email"],
   pkce: true,
