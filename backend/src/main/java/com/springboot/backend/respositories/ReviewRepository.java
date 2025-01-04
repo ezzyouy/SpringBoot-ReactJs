4269e87e73828c9c468b2276e59620aa22ebf495
ezzyouy <82385007+ezzyouy@users.noreply.github.com>
2025-01-04 19:18:08 +0100

    s23

--- backend/src/main/java/com/springboot/backend/respositories/ReviewRepository.java
+++ backend/src/main/java/com/springboot/backend/respositories/ReviewRepository.java
@@ -6,7 +6,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.RequestParam;
 
-public interface ReviewRepository extends JpaRepository<Review,Long> {
+public interface ReviewRepository extends JpaRepository<Review, Long> {
     Page<Review> findByBookId(@RequestParam("book_id") Long bookId, Pageable pageable);
 
+    Review findByUserEmailAndBookId(@RequestParam("user_email") String UserEmail, Long bookId);
 }
